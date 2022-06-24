052f2b8f6f09ca72b57bf639c2600a4f34741de6
KUSUM24 <2019kusum.rohra@ves.ac.in>
2022-06-24 21:54:21 +0530

    firebase connected

---
+++ database/firebase.js
@@ -0,0 +1,13 @@
+const firebase = require("firebase/app");
+require("firebase/firestore");
+require("firebase/storage");
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+firebase.initializeApp(firebaseConfig);
+module.exports = firebase;
